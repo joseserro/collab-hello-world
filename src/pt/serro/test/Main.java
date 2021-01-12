2f594d90c948249ae2a6aea3272ffed2d87379bb
Ze <jose.serro@appx.pt>
2021-01-12 12:18:33 +0000

    Initial commit, definição do main, hello world e criada estrutura para a soma

---
+++ src/pt/serro/test/Main.java
@@ -0,0 +1,14 @@
+package pt.serro.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 5;
+        int b = 10;
+        System.out.println("Soma de a com b = " + add(a, b));
+    }
+
+    private static int add(int a, int b) {
+        return 0;
+    }
+}
